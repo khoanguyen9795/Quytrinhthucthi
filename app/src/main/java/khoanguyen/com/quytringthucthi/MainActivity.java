dad38bc967cd948932aa7abf8b99a60d2dff98c3
khoanguyen9795 <khoanguyen438@gmail.com>
2020-04-10 21:32:59 +0700

    anh xa ID

--- app/src/main/java/khoanguyen/com/quytringthucthi/MainActivity.java
+++ app/src/main/java/khoanguyen/com/quytringthucthi/MainActivity.java
@@ -3,12 +3,29 @@ package khoanguyen.com.quytringthucthi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText mEdtSothunhat,mEdtSothuhai;
+    Button mBtnReset,mBtnCong,mBntTru,mBtnNhan,BtnChia;
+    TextView mTvKetqua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEdtSothunhat = findViewById(R.id.edittextSothunhat);
+        mEdtSothuhai = findViewById(R.id.edittextSothuhai);
+        mBtnReset = findViewById(R.id.buttonReset);
+        mBtnCong = findViewById(R.id.buttonCong);
+        mBntTru = findViewById(R.id.buttonTru);
+        mBtnNhan = findViewById(R.id.buttonNhan);
+        BtnChia = findViewById(R.id.buttonChia);
+        mTvKetqua = findViewById(R.id.textviewKetqua);
+        int a = 5;
+        a++;
+        Log.d("BBB",String.valueOf(a));
     }
 }
